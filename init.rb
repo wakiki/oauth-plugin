12741db881301ebe18def82842aa6cfbbf6ee7ca
Steve Leung <scyl88@gmail.com>
2010-09-17 18:39:23 +0100

    made compat with rails 3

--- init.rb
+++ init.rb
@@ -1,1 +1,11 @@
-require File.dirname(__FILE__) + "/rails/init"  
+gem 'oauth', '>=0.3.5'
+require 'oauth/signature/hmac/sha1'
+if Rails.version =~ /^3\./
+  require 'oauth/request_proxy/rack_request'
+else
+  require 'oauth/request_proxy/action_controller_request'
+end
+require 'oauth/server'
+require 'oauth/controllers/application_controller_methods'
+
+ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
